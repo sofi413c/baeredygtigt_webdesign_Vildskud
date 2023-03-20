b883eae808a6a0778d7bd3da3d432f483b4b26dd
Faith Mansilungan <faith0094@stud.kea.dk>
2023-03-20 12:42:12 +0100

    Poppins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],}
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
