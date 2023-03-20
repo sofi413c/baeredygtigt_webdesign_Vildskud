d0f65f5d3ebd603926423d9759afa97460d66665
Faith Mansilungan <faith0094@stud.kea.dk>
2023-03-20 13:05:34 +0100

    Farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        custom_green: "#C8FFA3",
+        custom_purple: "#82425F",
+        gradient_green: "#8A9B4D",
+        gradient_orange: "#D07F4E",
+        gradient_purple: "#82425F",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
