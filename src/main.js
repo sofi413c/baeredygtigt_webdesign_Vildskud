26e359aab361f12c4b5a3d0808f67e134b26b424
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2023-03-23 10:42:53 +0100

    header rettet

--- src/main.js
+++ src/main.js
@@ -22,7 +22,6 @@ function toggleMenu() {
 
 hamburger.addEventListener("click", toggleMenu);
 
-
 //SINGLEVIEW TOGGLE BTN//
 
 const singleview = document.querySelector("#singleview");
@@ -30,6 +29,19 @@ const beskrivelse = document.querySelector(".beskrivelse");
 const toggle_btn = document.querySelector(".toggle_btn");
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
+
+const open2 = document.querySelector(".open2");
+const close2 = document.querySelector(".close2");
+const singleview2 = document.querySelector("#singleview2");
+const tid_sted = document.querySelector(".tid_sted");
+const toggle_btn2 = document.querySelector(".toggle_btn2");
+
+
+const close3 = document.querySelector(".close3");
+const open3 = document.querySelector(".open3");
+const singleview3 = document.querySelector("#singleview3");
+const toggle_btn3 = document.querySelector(".toggle_btn3");
+const medvirkende = document.querySelector(".medvirkende");
 
 function toggle_knap() {
   console.log("not");
@@ -44,8 +56,31 @@ function toggle_knap() {
     open.style.display = "none";
   }
 }
+function toggle_knap2() {
+  if (singleview2.classList.contains("dropdown")) {
+    singleview2.classList.remove("dropdown");
+    close2.style.display = "none";
+    open2.style.display = "block";
+  } else {
+    console.log("yes");
+    singleview2.classList.add("dropdown");
+    close2.style.display = "block";
+    open2.style.display = "none";
+  }
+}
+function toggle_knap3() {
+  if (singleview3.classList.contains("dropdown")) {
+    singleview3.classList.remove("dropdown");
+    close3.style.display = "none";
+    open3.style.display = "block";
+  } else {
+    console.log("yes");
+    singleview3.classList.add("dropdown");
+    close3.style.display = "block";
+    open3.style.display = "none";
+  }
+}
 
 toggle_btn.addEventListener("click", toggle_knap);
-
-
-
+toggle_btn2.addEventListener("click", toggle_knap2);
+toggle_btn3.addEventListener("click", toggle_knap3);
